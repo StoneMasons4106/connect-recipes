f6610542118708cda75c120f66e8b4b3a7e2ed0e
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-06-15 22:36:25 -0400

    Customized the header to have the format I want, trimmed unnecessary html in the index page, added other html files to be filled in later.

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template, request, flash
 import os
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/new-recipe")
+def new_recipe():
+    return render_template("new_recipe.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(
