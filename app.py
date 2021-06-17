464dcb6d5dc58930a312883fa63352b9aaf5dd58
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-06-16 20:06:11 -0400

    Wired up connection to MongoDB, have some tags in Mongo displaying on index.html temporarily.

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template, request, flash
 import os
+from flask_pymongo import PyMongo
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    tags = list(mongo.db.tags.find())
+    return render_template("index.html", tags=tags)
 
 
 @app.route("/login")
